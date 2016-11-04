2bd54ee98973b71f208eac6007d84c7c3e287abb
tunnckoCore <mameto_100@mail.bg>
2016-11-05 01:39:16 +0200

    refactor(*): update boilerplate stuff

--- index.js
+++ index.js
@@ -1,61 +1,9 @@
 /*!
  * kind-of-extra <https://github.com/tunnckoCore/kind-of-extra>
  *
- * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
 'use strict'
 
-var isGens = require('is-es6-generators')
-var isStream = require('is-node-stream')
-var isError = require('is-typeof-error')
-var isPromise = require('is-promise')
-var isHybrid = require('is-hybrid')
-var kindOf = require('kind-of')
-
-/**
- * > Type check utility on steroids.
- *
- * **Example**
- *
- * ```js
- * kindof(123) // => 'number'
- * kindof('foo') // => 'string'
- * kindof({a: 'b'}) // => 'object'
- * kindof(new Error('foo')) // => 'error'
- * kindof(Promise.resolve(42)) // => 'promise'
- * kindof(function * () { yield 43 }) // => 'generatorfunction'
- * kindof(function * () { yield 43 }, false) // => 'function'
- * kindof((a, b) => { return a + b }) // => 'function'
- * ```
- *
- * @param  {Mixed} `val` value to check what type is
- * @param  {Boolean} `extra` when `false` would work as expected for `generator` and `generator function`
- * @return {String} type of the given `val`
- * @api public
- */
-module.exports = function kindOfExtra (val, extra) {
-  extra = typeof extra === 'boolean' ? extra : true
-
-  if (extra && isHybrid(val)) {
-    return 'hybrid'
-  }
-  if (isPromise(val)) {
-    return 'promise'
-  }
-  if (isStream(val)) {
-    return 'stream'
-  }
-  if (isError(val)) {
-    return 'error'
-  }
-  if (extra && isGens.isGenerator(val)) {
-    return 'generator'
-  }
-  if (extra && isGens.isGeneratorFunction(val)) {
-    return 'generatorfunction'
-  }
-
-  return kindOf(val)
-}
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * kind-of-extra <https://github.com/tunnckoCore/kind-of-extra>
  *
- * Copyright (c) 2015-2016 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
  * Released under the MIT license.
  */
 
@@ -9,69 +9,8 @@
 
 'use strict'
 
-var test = require('assertit')
-var kindof = require('./index')
-var through2 = require('through2')
-var hybridify = require('hybridify')
-var relike = require('relike')
-var fs = require('fs')
+var test = require('mukla')
 
-test('should work as `kind-of` sugar', function (done) {
-  test.strictEqual(kindof(123), 'number')
-  test.strictEqual(kindof('a'), 'string')
-  test.strictEqual(kindof({}), 'object')
-  test.strictEqual(kindof(/regex/), 'regexp')
-  done()
-})
-
-test('should work for Promises, Hybrids, Streams and Errors', function (done) {
-  var promise = relike(fs.readFile, './package.json', 'utf8')
-  var hybrid = hybridify(fs.readFile)('./package.json', 'utf8')
-  var stream = through2()
-  var error = new Error('foo')
-
-  test.strictEqual(kindof(promise), 'promise')
-  test.strictEqual(kindof(hybrid), 'hybrid')
-  test.strictEqual(kindof(stream), 'stream')
-  test.strictEqual(kindof(error), 'error')
-  done()
-})
-
-test('should work for es6 generators', function (done) {
-  var generator = (function * () { yield 42 })()
-  var genfn = function * () { yield 42 }
-
-  test.strictEqual(kindof(generator), 'generator')
-  test.strictEqual(kindof(genfn), 'generatorfunction')
-  done()
-})
-
-test('should treat hybrid as promise when `extra: false` (not as hybrid)', function (done) {
-  var hybrid = hybridify(fs.readFile)('./package.json', 'utf8')
-  test.strictEqual(kindof(hybrid, false), 'promise')
-  done()
-})
-
-test('should treat generator as object (as normally would) when `extra: false`', function (done) {
-  var generator = (function * () {
-    /* istanbul ignore next */
-    yield 55
-  })()
-  test.strictEqual(kindof(generator, false), 'object')
-  done()
-})
-
-test('should treat generator function as function (as normally) when `extra: false`', function (done) {
-  var genFunction = function * () {
-    /* istanbul ignore next */
-    yield [1, 2, 3]
-  }
-  test.strictEqual(kindof(genFunction, false), 'function')
-  done()
-})
-
-test('should treat arrow functions as function', function (done) {
-  var arrow = (a, b) => { return a * b }
-  test.strictEqual(kindof(arrow), 'function')
+test(function (done) {
   done()
 })
